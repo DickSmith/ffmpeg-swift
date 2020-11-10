ca10b21fee5dbd8d41fcec28e40f72647c0202d7
Dick Smith <git@dicksmith.ca>
2020-11-09 22:58:19 -0500

    split

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ffmpeg-swift",
-            targets: ["mobile-ffmpeg"]),
+            targets: ["expat", "fontconfig", "freetype"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,19 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "mobile-ffmpeg",
+            name: "expat",
+            url: "https://github.com/tanersener/mobile-ffmpeg/releases/download/v4.4/mobile-ffmpeg-full-gpl-4.4-ios-xcframework.zip",
+            checksum: "95d669c93bbbcf50c8730d6798b7f2ce4806b24ea9e24fc0d9f73863d76eba38"
+        ),
+
+        .binaryTarget(
+            name: "fontconfig",
+            url: "https://github.com/tanersener/mobile-ffmpeg/releases/download/v4.4/mobile-ffmpeg-full-gpl-4.4-ios-xcframework.zip",
+            checksum: "95d669c93bbbcf50c8730d6798b7f2ce4806b24ea9e24fc0d9f73863d76eba38"
+        ),
+
+        .binaryTarget(
+            name: "freetype",
             url: "https://github.com/tanersener/mobile-ffmpeg/releases/download/v4.4/mobile-ffmpeg-full-gpl-4.4-ios-xcframework.zip",
             checksum: "95d669c93bbbcf50c8730d6798b7f2ce4806b24ea9e24fc0d9f73863d76eba38"
         ),
